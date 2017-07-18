39a4c9309e875979b2c142a287a544e055292a3a
Paul Irish <paul.irish@gmail.com>
2017-07-18 10:46:49 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = require('express')()
 app.get('/', (req, res) => {
   const greeting = process.env.GREETING;
 
-  res.send(`${greeting || 'hello'} world!!33333333!`);
+  res.send(`${greeting || 'hello'} world!!333s33333!`);
 })
 
 app.listen(process.env.PORT || 3000)
